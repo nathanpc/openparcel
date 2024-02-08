e74a0809b004b3e9ba572b481dc7050785cf46f9
Nathan Campos <nathan@innoveworkshop.com>
2024-02-08 14:11:15 +0000

    Integrated the tracking into the Flask app
    Quick and dirty integration of the tracking system into the Flask web
    application for testing.

--- app.py
+++ app.py
@@ -2,13 +2,35 @@
 
 from flask import Flask
 
+import openparcel.carriers as carriers
+
+# Define the global flask application object.
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def hello_world():
+    return 'OpenParcel'
+
+
+@app.route('/track/<carrier_id>/<code>')
+def track(carrier_id: str, code: str):
+    """Tracks the history of a parcel given a carrier ID and a tracking code."""
+    # Get the requested carrier.
+    carrier = carriers.from_id(carrier_id)
+    if carrier is None:
+        return {
+            'title': 'Invalid carrier ID',
+            'message': 'Carrier ID doesn\'t match any of the available carriers.'
+        }, 422
+
+    # Fetch tracking history.
+    carrier = carrier(code)
+    data = carrier.fetch()
+
+    # Send tracking history to client.
+    return data
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
