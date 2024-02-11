2357fcde0d2dd8f960acea08b33439279135211c
Nathan Campos <nathan@innoveworkshop.com>
2024-02-11 12:10:22 +0000

    Started working on the database creation
    First steps into the creation of a database for the web application to
    use.

---
+++ sql/initialize.sql
@@ -0,0 +1,54 @@
+--- initialize.sql
+--- Initializes the OpenParcel database.
+---
+--- Nathan Campos <nathan@innoveworkshop.com>
+
+-- Ensure we at least have access to foreign keys.
+PRAGMA foreign_keys = ON;
+
+-- Users table.
+CREATE TABLE IF NOT EXISTS users(
+	id          INTEGER     PRIMARY KEY AUTOINCREMENT,
+	username    TEXT        NOT NULL UNIQUE,
+	password    TEXT        NOT NULL
+);
+
+-- Tracked parcels. Maintains a list of all the parcels tracked by the system.
+CREATE TABLE IF NOT EXISTS parcels(
+	id              INTEGER     PRIMARY KEY AUTOINCREMENT,
+	carrier         TEXT        NOT NULL,
+	tracking_code   TEXT        NOT NULL,
+	created         TIMESTAMP   NOT NULL
+);
+CREATE INDEX IF NOT EXISTS idx_parcels_carrier
+	ON parcels(carrier);
+CREATE UNIQUE INDEX IF NOT EXISTS idx_parcels_tracking_code
+	ON parcels(tracking_code);
+
+-- Relationship table for a user's tracked parcels.
+CREATE TABLE IF NOT EXISTS user_parcels(
+	user_id     INTEGER,
+	parcel_id   INTEGER,
+	delivered   BOOLEAN     NOT NULL    DEFAULT FALSE,
+
+	PRIMARY KEY (user_id, parcel_id),
+
+	FOREIGN KEY (user_id) REFERENCES users(id)
+		ON UPDATE CASCADE
+		ON DELETE CASCADE,
+	FOREIGN KEY (parcel_id) REFERENCES parcels(id)
+		ON UPDATE CASCADE
+		ON DELETE RESTRICT
+);
+
+-- Tracking history cache.
+CREATE TABLE IF NOT EXISTS history_cache(
+	id          INTEGER     PRIMARY KEY AUTOINCREMENT,
+	parcel_id   INTEGER     NOT NULL,
+	retrieved   TIMESTAMP   NOT NULL    DEFAULT CURRENT_TIMESTAMP,
+	data        JSON        NOT NULL,
+
+	FOREIGN KEY (parcel_id) REFERENCES parcels(id)
+		ON UPDATE CASCADE
+		ON DELETE CASCADE
+);
