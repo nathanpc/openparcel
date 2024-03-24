4b45ab88e2b4248966a6347d5c4d31f8139b3f77
Nathan Campos <nathan@innoveworkshop.com>
2024-03-24 11:01:26 +0000

    Refactored the proxy_list script for better database connection handling
    In order to allow for duplicate checking we would have to pass the
    database connection object around multiple places, which goes against
    the OOP principles, so we refactored multiple of its classes in order to
    store the connection internally and use it in multiple places.

--- bin/proxy_list.py
+++ bin/proxy_list.py
@@ -23,7 +23,8 @@ class Proxy:
     """A proxy server abstraction."""
 
     def __init__(self, addr: str, port: int, country: str, speed: int,
-                 protocol: str, active: bool = True, db_id: int = None):
+                 protocol: str, active: bool = True, db_id: int = None,
+                 conn: sqlite3.Connection = None):
         self.db_id: int = db_id
         self.addr: str = addr
         self.port: int = port
@@ -31,6 +32,7 @@ class Proxy:
         self.speed: int = speed
         self.protocol: str = protocol.lower()
         self.active: bool = active
+        self.conn: sqlite3.Connection = conn
 
     def test(self) -> bool:
         """Perform a cursory test of the proxy server."""
@@ -54,7 +56,7 @@ class Proxy:
 
                 start_time = time.time()
                 carrier.fetch()
-            except ScrapingReturnedError as e:
+            except ScrapingReturnedError:
                 speed = round((time.time() - start_time) * 1000)
                 timing.append(speed)
                 print(f'{speed} ms')
@@ -69,15 +71,36 @@ class Proxy:
 
         return True
 
-    def save(self, conn: sqlite3.Connection):
+    def is_duplicate(self) -> bool:
+        """Checks if we are already in the database."""
+        # Ensure we have a database connection.
+        if self.conn is None:
+            raise AssertionError('Database not available for duplicate check')
+
+        # Check the database if this object is already in it.
+        cur = self.conn.cursor()
+        cur.execute('SELECT id, active FROM proxies '
+                    'WHERE (addr = ?) AND (port = ?) AND (protocol = ?)',
+                    (self.addr, self.port, self.protocol))
+        row = cur.fetchone()
+        cur.close()
+
+        return row is not None
+
+    def save(self):
         """Saves the proxy information to our database."""
-        cur = conn.cursor()
+        # Ensure we have a database connection.
+        if self.conn is None:
+            raise AssertionError('Database not available for saving')
+
+        # Insert the object into the database.
+        cur = self.conn.cursor()
         cur.execute(
             'INSERT INTO proxies (addr, port, country, speed, protocol, active)'
             ' VALUES (?, ?, ?, ?, ?, ?)',
             (self.addr, self.port, self.country, self.speed, self.protocol,
              self.active))
-        conn.commit()
+        self.conn.commit()
         self.db_id = cur.lastrowid
         cur.close()
 
@@ -89,9 +112,10 @@ class Proxy:
 class ProxyList:
     """Generic proxy list provider class."""
 
-    def __init__(self, url: str):
+    def __init__(self, url: str, conn: sqlite3.Connection = None):
         self.url: str = url
         self.list: list[Proxy] = []
+        self.conn: sqlite3.Connection = conn
 
     def load(self) -> list[Proxy]:
         """Loads the proxy list from its source."""
@@ -115,7 +139,8 @@ class ProxyList:
 class PubProxy(ProxyList):
     """Proxy list using PubProxy as the backend."""
 
-    def __init__(self, api_key: str = None, country_denylist: list = None):
+    def __init__(self, api_key: str = None, country_denylist: list = None,
+                 conn: sqlite3.Connection = None):
         # Build up the request URL.
         url = 'http://pubproxy.com/api/proxy?format=json'
         if api_key is not None:
@@ -132,8 +157,12 @@ class PubProxy(ProxyList):
         url += '&cookies=true'
         url += '&referer=true'
 
-        super().__init__(url)
+        super().__init__(url, conn=conn)
         self.api_key: str = api_key
+
+        # Import API key from configuration.
+        if 'PUBPROXY_API_KEY' in config:
+            self.api_key = config['PUBPROXY_API_KEY']
 
     def load(self, num: int = 1) -> list[Proxy]:
         # Load the list multiple times in order to get a good selection.
@@ -146,33 +175,38 @@ class PubProxy(ProxyList):
         json = response.json()
         for item in json['data']:
             # Build up the proxy object.
-            proxy = Proxy(
+            server = Proxy(
                 addr=item['ip'],
                 port=int(item['port']),
                 country=item['country'],
                 speed=int(item['speed']) * 1000,
-                protocol=item['type'])
+                protocol=item['type'],
+                conn=self.conn)
 
-            # TODO: Check if we already have this proxy in our database.
+            # Check if we already have this proxy in our database.
+            if server.is_duplicate():
+                print(f'Duplicate proxy server {proxy.protocol}://{proxy.addr}:'
+                      f'{proxy.port} was ignored.')
+                continue
 
             # Test the proxy out before appending it to the list.
-            if proxy.test():
-                self.list.append(proxy)
+            if server.test():
+                self.list.append(server)
 
 
 if __name__ == '__main__':
     # Connect to the database.
-    conn = sqlite3.connect(dirname(dirname(abspath(__file__))) + '/' +
-                           config['DB_HOST'])
-    conn.execute('PRAGMA foreign_keys = ON')
+    db = sqlite3.connect(dirname(dirname(abspath(__file__))) + '/' +
+                         config['DB_HOST'])
+    db.execute('PRAGMA foreign_keys = ON')
 
     # Get a new proxy list.
-    proxies = PubProxy()
+    proxies = PubProxy(conn=db)
     proxies.load()
 
     # Save all the fetched proxies into the database.
     for proxy in proxies.list:
-        proxy.save(conn)
+        proxy.save()
 
     # Ensure we close the database connection.
-    conn.close()
+    db.close()
