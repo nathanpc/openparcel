552ed047d1f316da190572678915685e9ce0c36b
Nathan Campos <nathan@innoveworkshop.com>
2024-03-31 10:28:32 +0100

    Refactored DHL scraper to use classes
    The DHL scraper script now uses the new Javascript classes scraping
    system.

--- scrapers/dhl.js
+++ scrapers/dhl.js
@@ -1,168 +1,156 @@
-"use strict";
+class Carrier extends OpenParcel {
+	errorCheck() {
+		const errBlock = document.querySelector(
+			".c-tracking-result--status-shipment-undefined");
 
-/**
- * Checks for errors shown in the tracking page.
- *
- * @returns {any|null} OpenParcel error object or null if there weren't any
- *                     errors.
- */
-function errorCheck() {
-	const errBlock = document.querySelector(".c-tracking-result--status-shipment-undefined");
+		// Check for the existence of the error block element.
+		if (errBlock !== null) {
+			const errMessage = document.querySelector(
+				".c-tracking-result--info .c-tracking-result-message--content"
+			).innerText;
 
-	if (errBlock !== null) {
-		const errMessage = document.querySelector(".c-tracking-result--info .c-tracking-result-message--content").innerText;
-
-		if (errMessage.includes("tracking attempt was not successful")) {
-			return OpenParcel.error.create(OpenParcel.error.codes.ParcelNotFound);
-		} else if (errMessage.includes("tracking code is invalid")) {
-			return OpenParcel.error.create(OpenParcel.error.codes.InvalidTrackingCode);
-		}
-
-		return OpenParcel.error.create(OpenParcel.error.codes.Unknown);
-	}
-
-	return null;
-}
-
-/**
- * Performs the scraping operation.
- *
- * @return OpenParcel response object.
- */
-function scrape() {
-	let data = OpenParcel.data.create();
-
-	// Check for errors.
-	const error = errorCheck();
-	if (error !== null)
-		return error;
-
-	// Get tracking code, origin, and destination addresses.
-	data.trackingCode = document.querySelector(".c-tracking-result--code").innerText.split(":")[1].trim();
-	const addressRegex = new RegExp("[^:]+[\s:]+(.+)", "");
-	data.origin = parseAddress(addressRegex.exec(
-		document.querySelector(".c-tracking-result--origin").innerText)[1].trim());
-	data.destination = parseAddress(addressRegex.exec(
-		document.querySelector(".c-tracking-result--destination").innerText)[1].trim());
-
-	// Parse tracking history.
-	document.querySelectorAll(".c-tracking-result--checkpoint-info").forEach(function (checkpoint) {
-		let date = null;
-		checkpoint.querySelectorAll("li").forEach(function (item, index) {
-			// First item contains the date.
-			if (index === 0) {
-				const dateStr = item.querySelector(".c-tracking-result--checkpoint-date").innerText;
-				const match = dateStr.match(/(\w+), (\d+) (\d+)/);
-
-				date = new Date(
-					Number(match[3]),  // Year
-					OpenParcel.calendar.getMonth(match[1]),  // Month
-					Number(match[2])   // Day
-				);
+			// Parse the message contained within.
+			if (errMessage.includes("tracking attempt was not successful")) {
+				return new ParcelError(ParcelError.Code.ParcelNotFound);
+			} else if (errMessage.includes("tracking code is invalid")) {
+				return new ParcelError(ParcelError.Code.InvalidTrackingCode);
 			}
 
-			// Build up the tracking history update object.
-			data.history.push(parseHistoryItem(item, date));
+			// Looks like it's a new message that we've never seen before.
+			return new ParcelError(ParcelError.Code.Unknown);
+		}
+
+		return null;
+	}
+
+	scrape() {
+		const base = this;
+
+		// Check for errors.
+		const error = this.errorCheck();
+		if (error !== null)
+			return error;
+
+		// Get tracking code, origin, and destination addresses.
+		this.parcel.trackingCode = document.querySelector(".c-tracking-result--code")
+			.innerText.split(":")[1].trim();
+		const addressRegex = new RegExp("[^:]+[\s:]+(.+)", "");
+		this.parcel.origin = this.parseAddress(addressRegex.exec(
+			document.querySelector(".c-tracking-result--origin").innerText)[1].trim());
+		this.parcel.destination = this.parseAddress(addressRegex.exec(
+			document.querySelector(".c-tracking-result--destination").innerText)[1].trim());
+
+		// Parse tracking history.
+		document.querySelectorAll(".c-tracking-result--checkpoint-info").forEach((checkpoint) => {
+			let date = null;
+			checkpoint.querySelectorAll("li").forEach((item, index) => {
+				// First item contains the date.
+				if (index === 0) {
+					const dateStr = item.querySelector(
+						".c-tracking-result--checkpoint-date").innerText;
+					const match = dateStr.match(/(\w+), (\d+) (\d+)/);
+
+					date = new ParcelTimestamp(new Date(
+						Number(match[3]),  // Year
+						ParcelTimestamp.getMonthIndex(match[1]),  // Month
+						Number(match[2])   // Day
+					));
+				}
+
+				// Build up the tracking history update object.
+				base.parcel.appendUpdate(base.parseHistoryItem(item, date.clone()));
+			});
 		});
-	});
 
-	// Set the current delivery status.
-	if (data.history.length > 0)
-		data.status = data.history[0].status;
+		return this.parcel;
+	}
 
-	return OpenParcel.data.finalTouches(data);
-}
+	/**
+	 * Parses the addresses in any of the formats used by DHL.
+	 *
+	 * @param {string} line Address line from an element.
+	 *
+	 * @return {ParcelLocation} OpenParcel address object.
+	 */
+	parseAddress(line) {
+		const address = new ParcelLocation();
+		const parts = line.replace(/[\r\n\t]+/g, " ").split(" - ");
 
-/**
- * Parses the addresses in any of the formats used by DHL.
- *
- * @param {string} line Address line from an element.
- *
- * @return OpenParcel address object.
- */
-function parseAddress(line) {
-	const address = OpenParcel.data.createAddress();
-	const parts = line.replace(/[\r\n\t]+/g, " ").split(" - ");
+		// Non-detailed address.
+		if (parts.length === 1) {
+			address.country = parts[0].trim();
+			return address;
+		}
 
-	// Non-detailed address.
-	if (parts.length === 1) {
-		address.country = parts[0].trim();
+		// Detailed address.
+		address.country = parts[parts.length - 1].trim();
+		address.state = parts[parts.length - 2].trim();
+		if (parts.length >= 3) {
+			address.city = parts[parts.length - 3].trim().replace(
+				"Service Area: ", "");
+		}
+
 		return address;
 	}
 
-	// Detailed address.
-	address.country = parts[parts.length - 1].trim();
-	address.state = parts[parts.length - 2].trim();
-	if (parts.length >= 3) {
-		address.city = parts[parts.length - 3].trim().replace(
-			"Service Area: ", "");
+	/**
+	 * Parses a tracking history item.
+	 *
+	 * @param {Element} 		item HTML element of the update item block.
+	 * @param {ParcelTimestamp} date Timestamp when this update occurred.
+	 *
+	 * @returns {ParcelUpdate} OpenParcel tracking history item object.
+	 */
+	parseHistoryItem(item, date) {
+		// Set the appropriate time.
+		const timeStr = item.querySelector(
+			".c-tracking-result--checkpoint-time").innerText;
+		const match = timeStr.match(/(\d+):(\d+)/);
+		if (match !== null)
+			date.setTime(Number(match[1]), Number(match[2]));
+
+		// Create the update object.
+		const details = item.querySelector(
+			".c-tracking-result--checkpoint-right p.bold");
+		const update = new ParcelUpdate(details.innerText, null, null, date);
+
+		// Get the update location.
+		const locStr = item.querySelector(".c-tracking-result--checkpoint--more").innerText;
+		if (locStr.length > 0)
+			update.location = this.parseAddress(locStr);
+
+		// Check if we have anything for the description.
+		const noteMatch = update.title.match(/(.+)\s+Note[\s:-]+(.+)/);
+		if (noteMatch !== null) {
+			update.title = noteMatch[1].trim();
+			update.description = noteMatch[2].trim();
+		}
+
+		// Trim out any weird characters from title.
+		if (update.title.endsWith("."))
+			update.title = update.title.slice(0, -1)
+
+		// Check if it was delivered.
+		if (update.title.containsAny(["Delivered", "Delivery successful"])) {
+			update.status = new ParcelStatus(ParcelStatus.Type.Delivered, update.title, {
+				to: null
+			});
+		} else if (update.title.containsAny(["Being delivered", "with courier"])) {
+			update.status = new ParcelStatus(ParcelStatus.Type.Delivering, update.title);
+		} else if (update.title.containsAny(["picked up", "posted"])) {
+			update.status = new ParcelStatus(ParcelStatus.Type.Posted, update.title);
+		} else if (update.title.includes("electronically")) {
+			update.status = new ParcelStatus(ParcelStatus.Type.Created, update.title, {
+				timestamp: null
+			});
+		} else if (update.title.includes("Clearance processing complete")) {
+			update.status = new ParcelStatus(ParcelStatus.Type.CustomsCleared, update.title);
+		} else if (update.title.includes("Delivery not possible")) {
+			update.status = new ParcelStatus(ParcelStatus.Type.DeliveryAttempt, update.title);
+		} else if (update.title.includes("on hold")) {
+			update.status = new ParcelStatus(ParcelStatus.Type.Issue, update.title);
+		}
+
+		return update;
 	}
-
-	return address;
-}
-
-/**
- * Parses a tracking history item.
- *
- * @param {Element} item HTML element of the update item block.
- * @param {Date}    date Date and time that this update occurred.
- *
- * @returns OpenParcel tracking history item object.
- */
-function parseHistoryItem(item, date) {
-	// Set the appropriate time.
-	const timeStr = item.querySelector(".c-tracking-result--checkpoint-time").innerText;
-	const match = timeStr.match(/(\d+):(\d+)/);
-	if (match !== null)
-		date.setUTCHours(Number(match[1]), Number(match[2]));
-
-	// Create the update object.
-	const details = item.querySelector(".c-tracking-result--checkpoint-right p.bold");
-	const update = OpenParcel.data.createUpdate(details.innerText, null, {
-		timestamp: date.toISOString()
-	});
-
-	// Get the update location.
-	const locStr = item.querySelector(".c-tracking-result--checkpoint--more").innerText;
-	if (locStr.length > 0)
-		update.location = parseAddress(locStr);
-
-	// Check if we have anything for the description.
-	const noteMatch = update.title.match(/(.+)\s+Note[\s:-]+(.+)/);
-	if (noteMatch !== null) {
-		update.title = noteMatch[1].trim();
-		update.description = noteMatch[2].trim();
-	}
-
-	// Trim out any weird characters from title.
-	if (update.title.endsWith("."))
-		update.title = update.title.slice(0, -1)
-
-	// Check if it was delivered.
-	if (update.title.includes("Delivered") || update.title.includes("Delivery successful")) {
-		update.status = OpenParcel.data.createStatus("delivered", update.title, {
-			to: null
-		});
-	} else if (update.title.includes("Being delivered") || update.title.includes("with courier")) {
-		update.status = OpenParcel.data.createStatus("delivering",
-			update.title);
-	} else if (update.title.includes("picked up") || update.title.includes("posted")) {
-		update.status = OpenParcel.data.createStatus("posted",
-			update.title);
-	} else if (update.title.includes("electronically")) {
-		update.status = OpenParcel.data.createStatus("created", update.title, {
-			timestamp: null
-		});
-	} else if (update.title.includes("Clearance processing complete")) {
-		update.status = OpenParcel.data.createStatus("customs-cleared`",
-			update.title);
-	} else if (update.title.includes("Delivery not possible")) {
-		update.status = OpenParcel.data.createStatus("delivery-attempt",
-			update.title);
-	} else if (update.title.includes("on hold")) {
-		update.status = OpenParcel.data.createStatus("issue",
-			update.title);
-	}
-
-	return update;
 }
