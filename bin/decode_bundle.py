25a44406615a1396071d4371ed211afc0da557d9
Nathan Campos <nathan@innoveworkshop.com>
2024-04-17 12:21:11 +0100

    Decoding carrier request bundles from stdin
    Our carrier request bundle decoding script now fully supports reading
    request bundles from stdin.

--- bin/decode_bundle.py
+++ bin/decode_bundle.py
@@ -2,9 +2,41 @@
 
 import base64
 import hashlib
+import sys
 
 from Crypto.Cipher import AES
 from Crypto.Util import Counter
+
+
+class InputLineStream:
+    """Iterator that allows us to always read an entire line from a stream."""
+
+    def __init__(self, in_stream = sys.stdin):
+        self.in_stream = in_stream
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        line = ''
+
+        # Read an entire line from the input stream.
+        while True:
+            c = sys.stdin.read(1)
+
+            # Check for operation ending characters.
+            if c == '':
+                # Check if we've hit EOF.
+                raise StopIteration
+            elif c == '\n':
+                # Have we reached the end of a line?
+                return line
+            elif c == '\r':
+                # Ignored characters.
+                continue
+
+            # Append the character to our buffer.
+            line += c
 
 
 class RequestBundle:
@@ -29,10 +61,36 @@ class RequestBundle:
         # Decrypt the
         return cipher.decrypt(raw[AES.block_size:]).decode('utf-8')
 
+    def read_bundle(self, line_stream: InputLineStream = InputLineStream()):
+        """Reads a bundle from a line buffer."""
+        reading_bundle = False
+        enc_bundle = ''
+
+        for line in line_stream:
+            # Check if the next line will contain the actual bundle data.
+            if line == '-----BEGIN OPENPARCEL BUNDLE-----':
+                reading_bundle = True
+                continue
+
+            # Are we actually reading the bundle data?
+            if reading_bundle:
+                # Have we finished reading a bundle?
+                if line == '------END OPENPARCEL BUNDLE------':
+                    break
+
+                # Store the snippet of data we just got.
+                enc_bundle += line
+
+        # Decode the bundle data.
+        bundle = self.decrypt(enc_bundle)
+
+        return bundle
+
 
 if __name__ == '__main__':
-    b64_str = 'put base64 text here'
     key = 'please change me'  # TODO: Read from config.
 
+    # Read the bundle and print its decoded output.
     bc = RequestBundle(key)
-    print(bc.decrypt(b64_str))
+    data = bc.read_bundle()
+    print(f'\n{data}')
